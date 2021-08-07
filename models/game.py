1b3f4cce82941e011e57162691c5a7d965166c15
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-07 21:59:19 +0100

    create GPlayer & Game class

---
+++ models/game.py
@@ -0,0 +1,10 @@
+from models.player import Player
+
+class Game():
+
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def play():
+        pass
