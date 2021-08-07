ba65c9ef26fee385f50058432e69045f3d28e49e
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-07 22:34:06 +0100

    create routes

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,4 +5,18 @@ from models.game import Game
 
 @app.route('/')
 def index():
-    return 'Hello!'
+    return render_template('index.html')
+
+@app.route('/rules')
+def rules():
+    return 'rules!'
+
+@app.route('/<player1>/<player2>')
+def play_game(player1, player2):
+    player_1 = Player('Player 1', player1.lower())
+    player_2 = Player('Player 2', player2.lower())
+    game = Game(player_1, player_2)
+    result = game.play()
+    return render_template('game.html', result = result)
+    
+# rock, paper, scissors
