dfa5a60646dce8045826a36fc057dc923bd854dc
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-08 00:09:59 +0100

    create  route to play the game

--- controllers/controller.py
+++ controllers/controller.py
@@ -13,5 +13,10 @@ def rules():
 
 @app.route('/<player1>/<player2>')
 def play_game(player1, player2):
-    return 'play!'
+    player_1 = Player('Player 1', player1.lower())
+    player_2 = Player('Player 2', player2.lower())
+    game = Game(player_1, player_2)
+    result = game.play()
+    return render_template('game.html', result = result)
     
+# rock, paper, scissors
