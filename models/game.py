0988bf1445caba78df4bce8779c17cbf26a4bd20
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-08 00:08:45 +0100

    create player fnc

--- models/game.py
+++ models/game.py
@@ -5,6 +5,17 @@ class Game():
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+        # self.result = {1: 'win', 2: 'lose', 3: 'draw'}
 
-    def play():
-        pass
+    def play(self):
+        if self.player_1.move == self.player_2.move:
+            return None
+        elif self.player_1.move == 'rock' and self.player_2.move == 'paper':
+            return self.player_2
+        elif self.player_1.move == 'paper' and self.player_2.move == 'scissors':
+            return self.player_2
+        elif self.player_1.move == 'scissors' and self.player_2.move == 'rock':
+            return self.player_2
+        else:
+            return self.player_1
+    # rock, paper, scissors
