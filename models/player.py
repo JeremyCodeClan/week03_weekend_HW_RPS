6a9cf2b640985ea4659eaf981a0c599b5fdf3ebe
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-08 23:06:54 +0100

    create initialize, random_pick, win_game fnc in player class

--- models/player.py
+++ models/player.py
@@ -1,5 +1,24 @@
+import random
+
 class Player():
 
-    def __init__(self, name, move):
+    def __init__(self, name = '', move = ''):
         self.name = name
         self.move = move
+        self.score = 0
+        self.draw = 0
+        self.options = ['rock', 'paper', 'scissors']
+
+    def initialize(self):
+        self.name = ''
+        self.move = ''
+        self.score = 0
+
+    def random_pick(self):
+        self.move = random.choice(self.options)
+
+    def win_game(self):
+        self.score += 1
+
+    def draw_game(self):
+        self.draw += 1
